d6f7d067438f2169a9c397906966ba7562fe6e92
jose-M2020 <67127783+jose-M2020@users.noreply.github.com>
2021-05-20 13:07:10 -0500

    correcion del uri de MongoDB - 2

--- src/mongoDB/DBconnection.js
+++ src/mongoDB/DBconnection.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
 mongoose.Promise = require("bluebird");
 
-// mongodb://localhost/chat
-//mongodb+srv://jose:josepass20@cluster0.sqmod.mongodb.net/test   ->this URI is for connect using MongoDB Compass
+/*
+	Connect MongoDB in localhost: mongodb://localhost/chat
+	Connect MongoDB in cloud MongoDB - mlab: mongodb+srv://jose:josepass20@cluster0.sqmod.mongodb.net/chatDB?retryWrites=true&w=majority
+	Connect with uri from MongoDB Compass: mongodb+srv://jose:josepass20@cluster0.sqmod.mongodb.net/test
+*/
 
-const url = process.env.MONGODB || "mongodb+srv://jose:josepass20@cluster0.sqmod.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"; //conexion mongo
+const uri = process.env.MONGODB || "mongodb+srv://jose:josepass20@cluster0.sqmod.mongodb.net/chatDB?retryWrites=true&w=majority"; //conexion mongo
 mongoose.set('useFindAndModify', false);
 
-const connect = mongoose.connect(url, { 
+const connect = mongoose.connect(uri, { 
 	useNewUrlParser: true 
 }).then(db => console.log('DB is connected'))
   .catch(err => console.error(err));
