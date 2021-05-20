e90b049ec437b8fefa82d858a42907f1490ea038
jose-M2020 <67127783+jose-M2020@users.noreply.github.com>
2021-05-20 09:27:12 -0500

    correcion del url de MongoDB

--- src/mongoDB/DBconnection.js
+++ src/mongoDB/DBconnection.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 mongoose.Promise = require("bluebird");
 
 // mongodb://localhost/chat
-const url = process.env.MONGODB || "mongodb+srv://jose:josepass20@cluster0.sqmod.mongodb.net/test"; //conexion mongo
+//mongodb+srv://jose:josepass20@cluster0.sqmod.mongodb.net/test   ->this URI is for connect using MongoDB Compass
+
+const url = process.env.MONGODB || "mongodb+srv://jose:josepass20@cluster0.sqmod.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"; //conexion mongo
 mongoose.set('useFindAndModify', false);
 
 const connect = mongoose.connect(url, { 
